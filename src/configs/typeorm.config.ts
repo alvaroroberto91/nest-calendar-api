7c479bf028cb2164d77da0fa0e35f97f59ee27e9
Alvaro Ferreira Costa <aferrcos@emeal.nttdata.com>
2023-11-03 15:55:11 -0300

    Finish the project

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,8 +4,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     "type": "postgres",
     "host": "pgsql",
     "port": 5432,
-    "username": "admin",
-    "password": "kasekage",
+    "username": "your-db-username",
+    "password": "your-db-password",
     "database": "postgres",
     "synchronize": true,
     "entities": ["dist/**/*.entity{.ts,.js}"]
